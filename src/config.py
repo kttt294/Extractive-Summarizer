00aeb0e03df88281693b4feadeb309883fc5aa34
kttt294 <kieuthutrang2904@gmail.com>
2026-07-26 20:52:50 +0700

    update src

--- src/config.py
+++ src/config.py
@@ -10,8 +10,8 @@ MODEL_CONFIGS = {
 
 # Các siêu tham số tối ưu (Thu được từ quá trình Grid Search Evaluation)
 OPTIMAL_HYPERPARAMS = {
-    'alpha': 0.25,        # Tỷ lệ chọn câu K%
-    'theta': 0.88,        # Ngưỡng Cosine Similarity để lọc trùng ngữ nghĩa (Post-filtering)
+    'alpha': 0.15,        # Tỷ lệ chọn câu K%
+    'theta': 0.80,        # Ngưỡng Cosine Similarity để lọc trùng ngữ nghĩa (Post-filtering)
     'min_words': 4,       # Số từ tối thiểu cho một câu hợp lệ (Giữ được tiêu đề & lọc cụm rác <4 từ)
     'max_words': 90,      # Số từ tối đa cho một câu hợp lệ (Bao quát được các câu báo chí phức hợp)
     'buffer_k': 2         # Số lượng cụm đệm cho K-Means để bù đắp sau khi lọc trùng
